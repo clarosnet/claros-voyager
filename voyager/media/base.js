67a239514b9df0c0642c36b94a475ee36fcbc42c
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2011-05-17 15:42:36 +0100

    query at bottom of page now shows a little more and has a better cursor.

--- voyager/media/base.js
+++ voyager/media/base.js
@@ -23,10 +23,10 @@ function voyager_simple_map(id, longitude, latitude) {
 }
 
 $(function () {
-	$('#query').css('overflow', 'hidden').height(100).append($('<div id="query-fade"/>').bind('click', expand_query)).bind('click', expand_query);
+	$('#query').css('overflow', 'hidden').css('cursor', 'pointer').height(150).append($('<div id="query-fade"/>').bind('click', expand_query)).bind('click', expand_query);
 });
 	
 function expand_query() {
 	$('#query-fade').remove();
-	$('#query').css('height', 'auto').slideDown();
+	$('#query').css('height', 'auto').css('cursor', 'default').slideDown();
 }
